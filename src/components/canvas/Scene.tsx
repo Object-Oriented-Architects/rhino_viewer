50d0b74f45766290302070b1db65ef918d01c8d9
ljh <ljh@ljh.studio>
2025-11-05 11:51:57 +0900

    Fix dpr

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -11,6 +11,7 @@ export default function Scene({ ...props }) {
   return (
     <Canvas
       {...props}
+      dpr={[1, 1.75]}
       onCreated={(state) => {
         state.gl.toneMapping = THREE.ACESFilmicToneMapping
         // state.gl.toneMapping = THREE.AgXToneMapping
